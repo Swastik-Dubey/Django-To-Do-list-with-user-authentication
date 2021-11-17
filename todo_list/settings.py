84a400b237e97a1fabc03806cca3947b60576943
Swastik  Dubey <89216281+Swastik-Dubey@users.noreply.github.com>
2021-11-17 17:45:22 +0530

    hi

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'timetable',
+        'USER': 'root',
+        'PASSWORD':'Swastik@2003',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS':{
+            'sql_mode' : 'traditional',}
     }
 }
 
